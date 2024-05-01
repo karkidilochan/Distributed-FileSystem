4dbbdd1df6e0df1f13c63ba6ba4bb330a3b3b70a
karkidilochan <karkidilochan.dk@gmail.com>
2024-04-30 19:54:12 -0600

    started erasure coding

--- src/main/java/csx55/dfs/utils/ReedSolomon.java
+++ src/main/java/csx55/dfs/utils/ReedSolomon.java
@@ -1,59 +1,108 @@
-// package csx55.dfs.utils;
+package csx55.dfs.utils;
 
-// // import erasure.ReedSolomon;
+import java.nio.ByteBuffer;
+import java.util.Map;
 
-// public class ReedSolomon {
+import erasure.ReedSolomon;
 
-// /* this will take each chunk byte array and return encode total shard */
-// public static void encode() {
-// public final int DATA_SHARDS = 4;
-// public final int PARITY_SHARDS = 2;
-// public final int TOTAL_SHARDS = 6;
+public class ReedSolomon {
+    public static final int DATA_SHARDS = 4;
 
-// public final int BYTES_IN_INT = 4;
+    public static final int PARITY_SHARDS = 2;
+    public static final int TOTAL_SHARDS = 6;
 
-// // file size
-// int fileSize = (int) inputFile.length();
+    public static final int BYTES_IN_INT = 4;
 
-// // total size of the stored data = length of the payload size
-// int storedSize = fileSize + BYTES_IN_INT;
+    /* this will take each chunk byte array and return encode total shard */
+    public static void encode(byte[] chunk) {
+        /* this is the original data */
 
-// // size of a shard. Make sure all the shards are of the same size.
-// // In order to do this, you can padd 0s at the end.
-// // This particular code works for 4 data shards.
-// // Based on the numer of shards, use a appropriate way to
-// // decide on shard size.
-// int shardSize = (storedSize + DATA_SHARDS - 1) / DATA_SHARDS;
+        // file size
+        // int fileSize = (int) inputFile.length();
 
-// // Create a buffer holding the file size, followed by the contents of the
-// file
-// // (and padding if required)
-// int bufferSize = shardSize * DATA_SHARDS;
+        // total size of the stored data = length of the payload size
+        int storedSize = chunk.length + BYTES_IN_INT;
 
-// /* this is the place to copy the chunk into */
-// byte[] allBytes = new byte[bufferSize];
+        // size of a shard. Make sure all the shards are of the same size.
+        // In order to do this, you can padd 0s at the end.
+        // This particular code works for 4 data shards.
+        // Based on the number of shards, use a appropriate way to
+        // decide on shard size.
+        int shardSize = (storedSize + DATA_SHARDS - 1) / DATA_SHARDS;
 
-// /*
-// * You should implement the code for copying the file size, payload and
-// * padding into the byte array in here.
-// */
-// // Make the buffers to hold the shards.
-// byte[][] shards = new byte[TOTAL_SHARDS][shardSize];
+        // Create a buffer holding the file size, followed by the contents of the file
+        // (and padding if required)
+        int bufferSize = shardSize * DATA_SHARDS;
 
-// // Fill in the data shards
-// for (int i = 0; i < DATA_SHARDS; i++) {
-// System.arraycopy(allBytes, i * shardSize, shards[i], 0, shardSize);
-// }
+        /* this is the place to copy the chunk into */
+        byte[] allBytes = new byte[bufferSize];
 
-// // Use Reed-Solomon to calculate the parity. Parity codes
-// // will be stored in the last two positions in 'shards' 2-D array.
-// ReedSolomon reedSolomon = new ReedSolomon(DATA_SHARDS, PARITY_SHARDS);
-// reedSolomon.encodeParity(shards, 0, shardSize);
+        /*
+         * You should implement the code for copying the file size, payload and
+         * padding into the byte array in here.
+         */
+        ByteBuffer.wrap(allBytes).putInt(chunk.length);
+        System.arraycopy(chunk, 0, allBytes, BYTES_IN_INT, chunk.length);
 
-// // finally store the contents of the 'shards' 2-D array
-// }
+        // Make the buffers to hold the shards.
+        byte[][] shards = new byte[TOTAL_SHARDS][shardSize];
 
-// public static void decode() {
+        // Fill in the data shards
+        for (int i = 0; i < DATA_SHARDS; i++) {
+            System.arraycopy(allBytes, i * shardSize, shards[i], 0, shardSize);
+        }
 
-// }
-// }
+        // Use Reed-Solomon to calculate the parity. Parity codes
+        // will be stored in the last two positions in 'shards' 2-D array.
+        ReedSolomon reedSolomon = new ReedSolomon(DATA_SHARDS, PARITY_SHARDS);
+        reedSolomon.encodeParity(shards, 0, shardSize);
+
+        // finally store the contents of the 'shards' 2-D array
+        System.out.println(shards);
+    }
+
+    public static void decode(Map<Integer, byte[]> shardMap) {
+
+        // Read in any of the shards that are present.
+        // (There should be checking here to make sure the input
+        // shards are the same size, but there isn't.)
+        byte[][] shards = new byte[TOTAL_SHARDS][];
+        boolean[] shardPresent = new boolean[TOTAL_SHARDS];
+        int shardSize = 0;
+        int shardCount = 0;
+        // now read the shards from the persistance store
+        for (int i = 0; i < TOTAL_SHARDS; i++) {
+            // Check if the shard is available.
+            // If avaialbe, read its content into shards[i]
+            // set shardPresent[i] = true and increase the shardCount by 1.
+            if (shardMap.containsKey(i)) {
+                shardPresent[i] = true;
+                shards[i] = shardMap.get(i);
+                shardCount += 1;
+            }
+        }
+
+        // We need at least DATA_SHARDS to be able to reconstruct the file.
+        if (shardCount < DATA_SHARDS) {
+            return;
+        }
+
+        // Make empty buffers for the missing shards.
+        for (int i = 0; i < TOTAL_SHARDS; i++) {
+            if (!shardPresent[i]) {
+                shards[i] = new byte[shardSize];
+            }
+        }
+
+        // Use Reed-Solomon to fill in the missing shards
+        ReedSolomon reedSolomon = new ReedSolomon(DATA_SHARDS, PARITY_SHARDS);
+        reedSolomon.decodeMissing(shards, shardPresent, 0, shardSize);
+
+        byte[] allBytes = new byte[shardSize * DATA_SHARDS];
+        for (int i = 0; i < DATA_SHARDS; i++) {
+            System.arraycopy(shards[i], 0, allBytes, shardSize * i, shardSize);
+        }
+
+        System.out.println(allBytes);
+    }
+}
